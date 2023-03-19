840e5a89bea4c9bbd54925249119e6b2859a8c16
Alysson <alysson.paulino@hotmail.com>
2023-03-19 15:20:57 -0300

    Configuração Cidade

--- src/app/config/api.js
+++ src/app/config/api.js
@@ -3,10 +3,10 @@ import axios from 'axios';
 const api = axios.create({
     baseURL: "https://api-next.bohr.io/api",
     //baseURL: "http://192.168.0.147:9000/AreaCliente",
-    auth: {
-        username: process.env.REACT_APP_USUARIO,
-        password: process.env.REACT_APP_SENHA
-    }
+    // auth: {
+    //     username: 'Alysson',
+    //     password: 'Alysson@2022'
+    // }
 
 });
 
